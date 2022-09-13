c95627544a1cf187131d7b07d7c52de78f498134
escalandres <aescalaa1900@alumno.ipn.mx>
2022-09-12 22:54:07 -0500

    searching funcction added on index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
+const div = document.createElement("div");  // <div></div>
+
+     
+
 function searching(){
     const typeSearching = document.getElementById("type-searching").value;
     alert(typeSearching)
+    div.textContent = `Nombre: ${Andesita.name}; Tipo: ${Andesita.type}; Descripcion: ${Andesita.description} `; 
+    app.insertAdjacentElement("afterend", div);
+}
+
+const Andesita = {
+    name: 'Andesita',
+    type: 'ignous',
+    description: 'Roca volcánica, de grano fino. Se forma en bordes convergentes de placas litosféricas.'
 }
