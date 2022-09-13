79ef384b2076aa2c0947423c187f1ed9e7b126f2
escalandres <aescalaa1900@alumno.ipn.mx>
2022-09-12 23:33:57 -0500

    Add element function added

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,64 @@
-const div = document.createElement("div");  // <div></div>
-
-     
-
-function searching(){
-    const typeSearching = document.getElementById("type-searching").value;
-    alert(typeSearching)
-    div.textContent = `Nombre: ${Andesita.name}; Tipo: ${Andesita.type}; Descripcion: ${Andesita.description} `; 
-    app.insertAdjacentElement("afterend", div);
-}
-
+document.body.onload = addElement;
 const Andesita = {
     name: 'Andesita',
     type: 'ignous',
     description: 'Roca volcánica, de grano fino. Se forma en bordes convergentes de placas litosféricas.'
 }
+
+const Anortosita = {
+    name: 'Anortosita',
+    type: 'ignous',
+    description: 'Roca plutónica de carácter básico, que está formada casi exclusivamente por plagioclasas y que también se encuentra en la superficie lunar.'
+}
+
+const Anfibolita = {
+    name: 'Anfibolita',
+    type: 'metamorphics',
+    description: 'Carbón de color negro y brillante, con un contenido mayor al 95 % de carbono.'
+}
+
+const Antracita = {
+    name: 'Antracita',
+    type: 'sedimentary',
+    description: 'Carbón de color negro y brillante, con un contenido mayor al 95 % de carbono.'
+}
+
+const ignous = [Andesita, Anortosita]
+
+function addElement(name, type, description) {
+  // crea un nuevo div
+  // y añade contenido
+    if(type !== undefined){
+        var newDiv = document.createElement("div");
+        newDiv.className = 'rocks';
+        //   var newContent = document.createTextNode(`Nombre: ${Andesita.name}; Tipo: ${Andesita.type}; Descripcion: ${Andesita.description} `);
+        var newContent = document.createTextNode(`Nombre: ${name}; Tipo: ${type}; Descripcion: ${description} `);  
+        newDiv.appendChild(newContent); //añade texto al div creado.
+
+        // añade el elemento creado y su contenido al DOM
+        var currentDiv = document.getElementById("rockss");
+        document.body.insertBefore(newDiv, currentDiv);
+    }
+
+}
+
+function searching(){
+    const typeSearching = document.getElementById("type-searching").value;
+    alert(typeSearching)
+    if(typeSearching==="ignous"){
+        alert(ignous.length)
+        for(i=0;i<ignous.length;i++){
+            addElement(ignous[i].name, ignous[i].type, ignous[i].description);
+
+        }
+    }
+    else if(typeSearching==="metamorphics"){
+
+    }
+    else{
+
+    }
+    
+    addElement(Andesita.name, Andesita.type, Andesita.description);
+}
+
